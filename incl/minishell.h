2274a5e2242405734758651174251dfb8d031e71
eelaine <eelaine@student.hive.fi>
2024-12-04 14:16:06 +0200

    first draft header and main

---
+++ incl/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 14:10:58 by eelaine           #+#    #+#             */
+/*   Updated: 2024/12/04 14:14:58 by eelaine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/incl/libft.h"
+# include <fcntl.h>
+# include <errno.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
