3f86a8d9cd35d6ce9a302c53f705402e6b5e5e34
eelaine <eelaine@student.hive.fi>
2024-12-11 15:08:40 +0200

    added env_copy.c

---
+++ srcs/env_copy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_copy.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 14:43:14 by eelaine           #+#    #+#             */
+/*   Updated: 2024/12/11 15:04:41 by eelaine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/minishell.h"
+
+size_t	env_count_variables(char **env)
+{
+	size_t	i;
+
+	i = 0;
+	while (env && env[i])
+		i++;
+	return (i);
+}
+
+int	env_copy(char **env)
+{
+	size_t	i;
+	size_t	env_count;
+
+	env_count = env_count_variables(env) + 1;
+	mini->env = ft_calloc(env_count, (sizeof * mini->env));
+	if (!mini->env)
+		return (FAIL);
+	i = -1;
+	while (env[++i])
+	{
+		mini->env = ft_strdup(env[i]);
+		if (!mini->env[i])
+			return (FAIL);
+	}
+	return (SUCCESS);
+}
