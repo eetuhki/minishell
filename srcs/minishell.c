55e00bf117b36ed56f974731f38efc10cc354310
eelaine <eelaine@student.hive.fi>
2024-12-12 10:02:25 +0200

    added env_copy function call

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: eelaine <eelaine@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 14:06:34 by eelaine           #+#    #+#             */
-/*   Updated: 2024/12/11 14:31:25 by eelaine          ###   ########.fr       */
+/*   Updated: 2024/12/12 10:01:54 by eelaine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,11 @@ void	get_input(void)
 
 int	main(int ac, char **av, char **env)
 {
-	(void)env;
-
 	arg_check(ac, av);
 	g_mini = ft_calloc(1, sizeof(t_mini));
 	if (!g_mini)
 		return (1);
+	env_copy(g_mini, env);
 	get_input();
 	return (0);
 }
