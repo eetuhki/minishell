66de4011d498bf465e86b9e1d9c9f6b2bd64eb47
fsolomon <fsolomon@student.hive.fi>
2025-02-24 13:45:22 +0200

    handles a .. input to resolve to command not found

--- srcs/cmd_table_setup.c
+++ srcs/cmd_table_setup.c
@@ -68,7 +68,7 @@ static char	**build_cmd_table(t_cmd *cmd, char **env)
         printf("Token[%d] = %s and TYPE= %d \n", t, cmd->tokens[t].content, cmd->tokens[t].type);
         t++;
     } */
-	if (ft_strcmp(cmd->tokens[0].content, ".") != 0)
+	if (ft_strcmp(cmd->tokens[0].content, ".") != 0 && ft_strcmp(cmd->tokens[0].content, "..") != 0)
 		check_full_cmd_path(cmd_table, cmd, env);
 	return (cmd_table);
 }
