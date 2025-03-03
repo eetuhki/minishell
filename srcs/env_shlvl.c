dae711b492bf8255d8de7550a76cd02ae1cb64af
eelaine <eelaine@student.hive.fi>
2025-03-03 15:19:20 +0200

    added shlvl error message if new lvl >999

--- srcs/env_shlvl.c
+++ srcs/env_shlvl.c
@@ -1,5 +1,12 @@
 #include "../incl/minishell.h"
 
+static void	shlvl_too_high(int lvl)
+{
+	ft_putstr_fd("mini: warning: shell level (", 2);
+	ft_putnbr_fd(lvl, 2);
+	ft_putendl_fd(") too high, resetting to 1", 2);
+}
+
 int	env_shlvl(t_mini *mini)
 {
 	char	*shlvl;
@@ -9,8 +16,11 @@ int	env_shlvl(t_mini *mini)
 	if (!shlvl)
 		return (env_set_var(mini, "SHLVL", "1"));
 	new_lvl = ft_atoi(shlvl) + 1;
-	if (new_lvl >= INT_MAX)
+	if (new_lvl > 999)
+	{
+		shlvl_too_high(new_lvl);
 		new_lvl = 1;
+	}
 	shlvl = ft_itoa(new_lvl);
 	if (!shlvl)
 		return (FAIL);
