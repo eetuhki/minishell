b4a75d8eda9ef10d2fd2324e9e9e16d724e50342
fsolomon <fsolomon@student.hive.fi>
2025-03-06 14:37:49 +0200

    norminette execute.c and added, changed exec_with_pipes to return FAIL / SUCCESS

--- srcs/execute.c
+++ srcs/execute.c
@@ -2,17 +2,14 @@
 
 void	exec_command(t_mini *mini, char **cmds)
 {
-	if (!cmds || !cmds[0])
-		exit(1);
 	validate_cmd_access(mini, cmds[0]);
 	if (execve(cmds[0], cmds, mini->env) == -1)
 	{
-		//printf("mini: execve failed with ERRNO [%d] [%s]\n", errno, strerror(errno));
 		exec_fail(mini, cmds[0]);
 	}
 }
 
-int		exec_no_pipes(t_mini *mini)
+int	exec_no_pipes(t_mini *mini)
 {
 	pid_t	pid;
 	int		status;
@@ -22,9 +19,8 @@ int		exec_no_pipes(t_mini *mini)
 	if (builtin_only(mini->cmds_tbl[0][0]))
 	{
 		if (setup_redirs(mini, mini->cmds[0]) == FAIL)
-            return (FAIL);
+			return (FAIL);
 		builtin_exit = handle_builtin(mini, 0);
-		//printf ("Ret value from BUILTIN is [%d] \n", builtin_exit);
 		mini->exit_code = builtin_exit;
 		reset_std_fds(mini);
 		return (builtin_exit);
@@ -38,8 +34,9 @@ int		exec_no_pipes(t_mini *mini)
 	}
 	else
 		wait_single(mini, pid, &status);
-	return (0);
+	return (SUCCESS);
 }
+
 void	close_inherited_fds(t_mini *mini, int pipe_i)
 {
 	int	i;
@@ -56,23 +53,26 @@ void	close_inherited_fds(t_mini *mini, int pipe_i)
 	}
 }
 
+// If not first command, use previous pipe as stdin
+// If it's NOT the last command, redirect stdout to pipe
+// Handle file redirections
 void	child_process(t_mini *mini, int i)
 {
 	sig_init_child();
-	if (mini->in_fd != STDIN) // If not first command, use previous pipe as stdin
+	if (mini->in_fd != STDIN)
 	{
 		dup2(mini->in_fd, STDIN);
 		close_fd(&mini->in_fd);
 	}
-	if (i < mini->pipes)  // If it's NOT the last command, redirect stdout to pipe
+	if (i < mini->pipes)
 	{
 		dup2(mini->fd[1], STDOUT);
 		close_fd(&mini->fd[1]);
 	}
 	close_fd(&mini->fd[0]);
 	close_fd(&mini->fd[1]);
-	close_inherited_fds(mini , i);
-	handle_redirs(mini->cmds[i], true, mini); // Handle file redirections
+	close_inherited_fds(mini, i);
+	handle_redirs(mini->cmds[i], true, mini);
 	if (builtin_only(*mini->cmds_tbl[i]))
 	{
 		if (handle_builtin(mini, i) == FAIL)
@@ -86,7 +86,9 @@ void	child_process(t_mini *mini, int i)
 	free_and_exit(mini);
 }
 
-void	exec_with_pipes(t_mini *mini)
+// Create a new pipe for all except last command
+// Close last read end after loop
+int	exec_with_pipes(t_mini *mini)
 {
 	int		i;
 	pid_t	pid;
@@ -94,27 +96,36 @@ void	exec_with_pipes(t_mini *mini)
 	i = 0;
 	if (mini->pids)
 		free_ptr(mini->pids);
-    mini->pids = ft_calloc((size_t)(mini->pipes + 1), sizeof(pid_t));
-    if (!mini->pids)
-        return ;
-    while (i <= mini->pipes && mini->cmds[i])
-    {
-		if (i < mini->pipes && pipe(mini->fd) == -1)  // Create a new pipe for all except last command
-			return(perror("mini: pipe failed"));
+	mini->pids = ft_calloc((size_t)(mini->pipes + 1), sizeof(pid_t));
+	if (!mini->pids)
+		return (FAIL);
+	while (i <= mini->pipes && mini->cmds[i])
+	{
+		if (i < mini->pipes && pipe(mini->fd) == -1)
+		{
+			perror("mini: pipe failed");
+			return (FAIL);
+		}
 		pid = fork();
-		if (pid == 0)  // Child process
-			child_process(mini, i); 
+		if (pid < 0)
+		{
+			perror("mini: fork failed");
+			return (FAIL);
+		}
+		if (pid == 0)
+			child_process(mini, i);
 		else
 			handle_fds(mini, pid, i);
 		i++;
 	}
 	if (mini->in_fd != STDIN)
-    	close_fd(&mini->in_fd); // Close last read end after loop
-    wait_multi(mini);
+		close_fd(&mini->in_fd);
+	wait_multi(mini);
 	mini->in_fd = STDIN;
+	return (SUCCESS);
 }
 
-int		execute(t_mini *mini)
+int	execute(t_mini *mini)
 {
 	if (!mini || !mini->cmds_tbl || !*mini->cmds_tbl || !*mini->cmds_tbl[0])
 		return (0);
